762c6609d60e154dfb5ca40af0c17f521e9bc2d8
ronnmabunga <ronnmabunga@gmail.com>
2024-10-23 07:40:21 +0800

    Add color-mode toggling functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const htmlElement = document.documentElement;
 
+// Function that applies bootstrap-defined theming property onto the html element
+function applyTheme(theme) {
+    htmlElement.setAttribute("data-bs-theme", theme);
+    localStorage.setItem("theme", theme);
+}
+
+// Checks for saved theme in localStorage (from previous usage of the app)
+// If no previous configurartion, it also checks for system color mode settings
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme) {
+    applyTheme(savedTheme);
+} else {
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    applyTheme(prefersDarkMode ? "dark" : "light");
+}
+
+// Event listener for a "theme-toggle" button, which calls the applyTheme function when clicked
+document.getElementById("theme-toggle").addEventListener("click", () => {
+    const currentTheme = htmlElement.getAttribute("data-bs-theme");
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    applyTheme(newTheme);
+});
